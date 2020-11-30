6ec8692e00da78422a542b4e2028e6a632338ff8
Farewell_ <norththelittlefox@gmail.com>
2020-11-30 12:30:21 +0100

    Instantiate Cube

---
+++ Assets/Scripts/LevelParser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelParser : MonoBehaviour
+{
+    [SerializeField] private GameObject Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Prefab, this.gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
