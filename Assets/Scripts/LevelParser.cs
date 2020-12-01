20bad28b26b3926036c4f8bb2102303bf39a2b4c
Farewell_ <norththelittlefox@gmail.com>
2020-12-01 09:23:30 +0100

    Instantiation Grille Test
    Not working yet

--- Assets/Scripts/LevelParser.cs
+++ Assets/Scripts/LevelParser.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class LevelParser : MonoBehaviour
 {
-    [SerializeField] private GameObject Prefab;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private TextAsset _maps;
+    [SerializeField] private char[] _mapContent = null;
+
+    private int _mapWidth = 29;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(Prefab, this.gameObject.transform);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _mapContent = _maps.text.ToCharArray();
+        for (int i = 0; i < _mapContent.Length; i++)
+        {
+            if (_mapContent[i] == '|')
+            {
+                GameObject temp = Instantiate(_prefab, this.transform);
+                Vector3 pos = new Vector3(i % _mapWidth, (int)-(i / _mapWidth), 0);
+                temp.transform.localPosition += pos / 5;
+            }
+        }
     }
 }
