915bc5cc11753116aed62ae9cb3c4155d8170133
Farewell_ <norththelittlefox@gmail.com>
2020-12-07 19:26:21 +0100

    Instantiation done
    system about to change soon

--- Assets/Scripts/LevelParser.cs
+++ Assets/Scripts/LevelParser.cs
@@ -7,21 +7,25 @@ public class LevelParser : MonoBehaviour
     [SerializeField] private GameObject _prefab;
     [SerializeField] private TextAsset _maps;
     [SerializeField] private char[] _mapContent = null;
+    [SerializeField] private float _scale = 10;
 
-    private int _mapWidth = 29;
+    private const int _mapWidth = 28;
+    private const int _mapHeight = 31;
 
+    private int _mapIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         _mapContent = _maps.text.ToCharArray();
+
         for (int i = 0; i < _mapContent.Length; i++)
         {
             if (_mapContent[i] == '|')
             {
                 GameObject temp = Instantiate(_prefab, this.transform);
-                Vector3 pos = new Vector3(i % _mapWidth, (int)-(i / _mapWidth), 0);
-                temp.transform.localPosition += pos / 5;
+                Vector3 pos = new Vector3((i % _mapWidth) - ((_prefab.transform.localScale.x*_scale*_mapWidth) / 2 - _prefab.transform.localScale.x*_scale/2), (int)-(i / _mapWidth) + ((_prefab.transform.localScale.y*_scale*_mapHeight)/2), 0);
+                temp.transform.localPosition += pos / _scale;
             }
         }
     }
